53b38838ba20bdc8a90109a0cad22ef68f02c39e
Nadezhda Korshun <nadezhda_korshun@epam.com>
2015-02-05 20:02:25 -0800

    Fix db

--- linksharing/settings.py
+++ linksharing/settings.py
@@ -72,9 +72,9 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'myuser',
         'PASSWORD': 'password',
-        'HOST': 'localhost',
-        # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': '', # Set to empty string for default.
+        # 'HOST': 'localhost',
+        # # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        # 'PORT': '', # Set to empty string for default.
     }
     # 'default': {
     #     # 'ENGINE': 'django_mongodb_engine',
